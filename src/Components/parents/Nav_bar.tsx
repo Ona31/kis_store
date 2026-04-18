ccdefaacfb220dfddb2df4812e702e15001599de
Elisee dev <eliseeonyembo@gmail.com>
2026-04-18 12:15:38 +0200

    Corection et ajoute des components et resposive

--- src/Components/parents/Nav_bar.tsx
+++ src/Components/parents/Nav_bar.tsx
@@ -16,13 +16,13 @@ export default function  NavComponent(){
 
     return(
        <>
-       <header className=" flex z-10 justify-between fixed w-full py-2 px-24 bg-[var(--nor)] shadow-sm items-center">
+       <header className=" flex z-10 justify-between fixed w-full py-2 px-4 md:px-24 bg-[var(--nor)] shadow-sm items-center">
          <Logo style={"text-2xl font-bold  text-[var(--text)]"} stspan={"text-[var(--pr)]"}/>
-        <Menu style={"flex gap-8"} stylea={" flex items-center gap-2 text-[var(--text)]  text-md"}/>
+        <Menu style={"md:flex hidden gap-8"} stylea={" flex items-center gap-2 text-[var(--text)]  text-md"}/>
        <div className="flex items-center gap-4">
         <CgShoppingBag className="text-2xl" />
         
-         <Btn href={""} desc={"Nous contacter!"} style={"py-2 px-3 bg-[var(--pr)] text-white  rounded-md"}/>
+         <Btn href={""} desc={"Nous contacter!"} style={" md:flex hidden py-2 px-3 bg-[var(--pr)] text-white  rounded-md"}/>
        </div>
        </header>
        </>
